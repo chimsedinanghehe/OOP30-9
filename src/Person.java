523a919723492c044f185f5369202431fb897f44
chimsedinanghehe <23020515@vnu.edu.vn>
2024-09-30 16:23:55 +0700

    hehe

---
+++ src/Person.java
@@ -0,0 +1,15 @@
+public class Person {
+    private String name;
+    private int BirthYear;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
